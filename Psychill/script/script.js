98ec14cb64fe7168fc80a721979dd354afad46a2
William Ryan <mkonryan@gmail.com>
2017-07-30 13:04:17 -0400

    Add files via upload
    got the functions working properly. 
    Program now randomly generates a link to some chill music each time the button is pushed.

--- Psychill/script/script.js
+++ Psychill/script/script.js
@@ -1,11 +1,30 @@
-var myArray = [
-	1: 'https://www.youtube.com/watch?v=mQ8moLn3e-k',
-	2: 'https://www.youtube.com/watch?v=NpOtkhBcc8k',
-	3: 'https://www.youtube.com/watch?v=0SUCWB_0bqw'
-]
+// music array
 
+var selectSong = [
+	 "https://www.youtube.com/watch?v=mQ8moLn3e-k",
+	 "https://www.youtube.com/watch?v=NpOtkhBcc8k",
+	 "https://www.youtube.com/watch?v=0SUCWB_0bqw",
+	 "https://www.youtube.com/watch?v=mJkaGXFLO7U",
+	 "https://www.youtube.com/watch?v=ZK3U92URi_c",
+	 "https://www.youtube.com/watch?v=mUUc_APqHOE",
+	 "https://www.youtube.com/watch?v=b8OELzmpgZo&list=PLB9FB4E1138286D3C",
+	 "https://www.youtube.com/watch?v=jhvqQ9orRrs",
+	 "https://www.youtube.com/watch?v=gIxuJQ1HAmc",
+	 "https://www.youtube.com/watch?v=uL0mvPZuklM",
+	 "https://www.youtube.com/watch?v=guXMb7zLblM"
+];
+   
+// function to generate random link
 
-function openInNewTab(url) {
+var pickASong = function () {
+var todaysSong = selectSong[Math.floor(Math.random() * selectSong.length)];
+return todaysSong;
+};
+ 
+//pickASong() to call function and return random link from the array
+
+ // function to open a new tab when the button is clicked
+function openMusicInNewTab(url) {
   var win = window.open(url, '_blank');
   win.focus();
 }
