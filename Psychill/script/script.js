1770183fbc9708ee030e2fa651d12aafa33bf6d2
William Ryan <mkonryan@gmail.com>
2017-07-30 13:13:36 -0400

    Add files via upload

--- Psychill/script/script.js
+++ Psychill/script/script.js
@@ -1,5 +1,4 @@
 // music array
-
 var selectSong = [
 	 "https://www.youtube.com/watch?v=mQ8moLn3e-k",
 	 "https://www.youtube.com/watch?v=NpOtkhBcc8k",
@@ -13,18 +12,14 @@ var selectSong = [
 	 "https://www.youtube.com/watch?v=uL0mvPZuklM",
 	 "https://www.youtube.com/watch?v=guXMb7zLblM"
 ];
-   
 // function to generate random link
-
 var pickASong = function () {
 var todaysSong = selectSong[Math.floor(Math.random() * selectSong.length)];
 return todaysSong;
 };
- 
-//pickASong() to call function and return random link from the array
-
- // function to open a new tab when the button is clicked
+//'pickASong()' to call function and return random link from the array
+// function to open a new tab when the button is clicked
 function openMusicInNewTab(url) {
   var win = window.open(url, '_blank');
   win.focus();
-}
+};
